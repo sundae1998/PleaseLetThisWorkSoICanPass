93a1b5fe6db2262c01dbf90d2c13d159573ae127
GitHub Action <action@github.com>
2022-03-23 16:47:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sundae1998/pleaseletthisworksoicanpass/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sundae1998.pleaseletthisworksoicanpass.services
+
+import com.github.sundae1998.pleaseletthisworksoicanpass.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/sundae1998/pleaseletthisworksoicanpass/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sundae1998/pleaseletthisworksoicanpass/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sundae1998.pleaseletthisworksoicanpass.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sundae1998.pleaseletthisworksoicanpass.MyBundle
 
 class MyProjectService(project: Project) {
 
